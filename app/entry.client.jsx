f978c2f0896d761b1f65054dcebf6e0c5e139285
Asif Iqbal <ahmedasif157@gmail.com>
2026-08-30 06:09:00 +0000

    fix: skip hydration, use createRoot for client rendering

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -1,13 +1,8 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition } from "react";
-import { hydrateRoot, createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 startTransition(() => {
-  try {
-    hydrateRoot(document, <RemixBrowser />);
-  } catch (e) {
-    console.error("Hydration failed, falling back to client render:", e);
-    const root = createRoot(document);
-    root.render(<RemixBrowser />);
-  }
+  const root = createRoot(document);
+  root.render(<RemixBrowser />);
 });
